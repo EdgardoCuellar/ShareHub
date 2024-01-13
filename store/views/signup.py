44e08e1527f8cd94c2ead8ee124ee0ee5a60067e
Edgardo Cuellar Sanchez <45764390+EdgardoCuellar@users.noreply.github.com>
2024-01-13 18:31:21 +0100

    Imrpove base, login, register, homepage

--- store/views/signup.py
+++ store/views/signup.py
@@ -1,23 +1,23 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password
+from django.shortcuts import render, redirect
 from store.models.customer import Customer
 from store.models.category import Category
 from django.views import View
 
-
-class Signup (View):
+class Signup(View):
     def get(self, request):
         categories = Category.get_all_categories()
-        return render (request, 'signup.html', {'categories': categories})
+        return render(request, 'signup.html', {'categories': categories})
 
     def post(self, request):
         postData = request.POST
-        first_name = postData.get ('firstname')
-        last_name = postData.get ('lastname')
-        faculty = postData.get ('faculty')
-        email = postData.get ('email')
-        password = postData.get ('password')
-        # validation
+        first_name = postData.get('firstname')
+        last_name = postData.get('lastname')
+        faculty = postData.get('faculty')
+        email = postData.get('email')
+        password = postData.get('password')
+
+        # Validation
         value = {
             'first_name': first_name,
             'last_name': last_name,
@@ -25,24 +25,27 @@ class Signup (View):
         }
         error_message = None
 
-        customer = Customer (first_name=first_name,
-                             last_name=last_name,
-                             faculty=faculty,
-                             email=email,
-                             password=password)
+        customer = Customer(first_name=first_name,
+                            last_name=last_name,
+                            faculty=faculty,
+                            email=email,
+                            password=password)
 
         error_message = Customer.validateCustomer(customer)
 
         if not error_message:
-            customer.password = make_password (customer.password)
-            customer.register ()
+            # Hash the password before saving
+            hashed_password = make_password(customer.password)
+            customer.password = hashed_password
+
+            customer.register()
 
             request.session['customer'] = customer.id
-            return redirect ('product')
+            return redirect('product')
         else:
             data = {
                 'error': error_message,
                 'values': value,
                 'categories': Category.get_all_categories()
             }
-            return render (request, 'signup.html', data)
+            return render(request, 'signup.html', data)
