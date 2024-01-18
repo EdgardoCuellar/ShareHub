482488ba822d51985a0b374617431247bf381ca2
Edgardo Cuellar Sanchez <45764390+EdgardoCuellar@users.noreply.github.com>
2024-01-18 15:57:48 +0100

    test gitignore

--- ShareHub/settings.py
+++ ShareHub/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-95t%=#4o3@l-(-%ok9*h%n3!0(sdchjn%+_$5#umaj-!3bg*7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-DATABASE_SQLITE = False
+DATABASE_SQLITE = True
 
 ALLOWED_HOSTS = ['15.188.149.136', 'localhost', '127.0.0.1']
 
