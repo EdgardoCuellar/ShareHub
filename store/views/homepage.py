c4dd8ed575a2cadfc2f83b951e948665cfd9b62f
Edgardo Cuellar Sanchez <45764390+EdgardoCuellar@users.noreply.github.com>
2024-01-18 15:59:45 +0100

    update gitignore

--- store/views/homepage.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render , redirect
-
-from django.views import  View
-
-class Homepage(View):
-    def get(self , request):
-        
-        return render(request , 'homepage.html' , {'products' : False} )
