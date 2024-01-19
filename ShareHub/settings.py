7089d7a7de64bda2c24eafeb00c8f9b716a0a1b8
Edgardo Cuellar Sanchez <45764390+EdgardoCuellar@users.noreply.github.com>
2024-01-19 20:40:22 +0100

    pls work

--- ShareHub/settings.py
+++ ShareHub/settings.py
@@ -141,7 +141,7 @@ if DEBUG:
 else:
     STATIC_URL = 'http://127.0.0.1:5500/server/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    (os.path.join(BASE_DIR, 'static'), 'static'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
