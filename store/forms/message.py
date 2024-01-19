b3c6ea35336e662f908d68ecd6697729a9706269
Edgardo Cuellar Sanchez <45764390+EdgardoCuellar@users.noreply.github.com>
2024-01-19 19:56:07 +0100

    remove usless media

--- store/forms/message.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from store.models.message import Message
-
-class MessageForm(forms.ModelForm):
-    class Meta:
-        model = Message
-        fields = ['receiver', 'content']
